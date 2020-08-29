895496848fdd3da39331ee122f1abf774900d335
Fabio Tavares <fabreder@gmail.com>
2020-08-29 09:40:45 -0300

    170 - Componente Item Category

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:meals/components/category_item.dart';
+import 'package:meals/data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Vamos Cozinhar?"),
+            ),
+        body: GridView(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200, //largura máxima de cada elemento na linha
+            childAspectRatio: 3/2, //proporção entre altura e largura de cada elem.
+            crossAxisSpacing: 20, //espaço no eixo cruzado
+            mainAxisSpacing: 20, //espaço no eixo principal
+          ),
+          children: DUMMY_CATEGORIES.map((cat) {
+            return CategoryItem(cat);
+          }).toList(),
+        ),
+    );
+  }
+}
